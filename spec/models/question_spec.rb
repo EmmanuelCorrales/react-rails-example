7336a231b722dfe7da760ae7c46b5326686e3895
Emmanuel Corrales <contactme@emmanuelcorrales.com>
2017-07-31 04:49:44 +0800

    Test the model question for valid attributes.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with valid attributes" do
+    expect(Question.new).to be_valid
+  end
+
+  it "is not valid without content" do
+    question = Question.new(content: nil)
+    expect(question).to_not be_valid
+  end
+
+  it "is not valid without answer"do
+    question = Question.new(answer: nil)
+    expect(question).to_not be_valid
+  end
+
 end
