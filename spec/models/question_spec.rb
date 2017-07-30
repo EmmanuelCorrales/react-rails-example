0073ebd63548f6698c2135a8360d561cf6182fc6
Emmanuel Corrales <contactme@emmanuelcorrales.com>
2017-07-31 04:55:49 +0800

    Make the unit test pass by initializing the question model with attributes content and answer.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
 
   it "is valid with valid attributes" do
-    expect(Question.new).to be_valid
+    question = Question.new(content: "What is your name?", answer: "Emmanuel Corrales")
+    expect(question).to be_valid
   end
 
   it "is not valid without content" do
