f5b93b5cc2b89209012965de4d3014233a0edd97
Emmanuel Corrales <contactme@emmanuelcorrales.com>
2017-08-03 16:03:21 +0800

    Implement deleting a question.

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -23,11 +23,31 @@ var Body = React.createClass({
     this.setState({ questions: newState });
   },
 
+  handleDelete(id) {
+    console.log('Delete question clicked.');
+    $.ajax({
+      url: `/api/questions/${id}`,
+      type: 'DELETE',
+      success: () => {
+        console.log('Successfull deleted a question.');
+        this.removeQuestion(id);
+      }
+    });
+  },
+
+  removeQuestion(id) {
+    var newQuestions = this.state.questions.filter((question) => {
+      return question.id != id;
+    });
+
+    this.setState({ questions: newQuestions});
+  },
+
   render() {
     return (
       <div>
         <NewQuestion handleSubmit={this.handleSubmit} />
-        <Questions questions={this.state.questions} />
+        <Questions questions={this.state.questions} handleDelete={this.handleDelete} />
       </div>
     )
   }
--- app/assets/javascripts/components/_questions.js.jsx
+++ app/assets/javascripts/components/_questions.js.jsx
@@ -1,9 +1,15 @@
 var Questions = React.createClass({
+
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+
   render() {
 		var questions = this.props.questions.map((question) => {
 			return (
 				<div key={question.id}>
 					<h3>{question.content}</h3>
+          <button onClick={this.handleDelete.bind(this, question.id)} >Delete</button>
 				</div>
 			)
 		});
