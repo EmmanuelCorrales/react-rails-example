08d4b0a68746239e169e183307fdbf651185401c
Emmanuel Corrales <contactme@emmanuelcorrales.com>
2017-08-04 14:12:13 +0800

    Add test data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Question.delete_all
+Question.create(content: "1 + 1?", answer: "2")
+Question.create(content: "Ruby or PHP?", answer: "Ruby")
+Question.create(content: "From what country is the Eiffel Tower located?", answer: "France")
+Question.create(content: "How many are the first gen pokemons?", answer: "151")
+Question.create(content: "What is the color of the White House?", answer: "White")
